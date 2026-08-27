1a1bb9b94d1bdc474eebe48da3f0f03395b84c3c
Tien Le <71688550+tientle@users.noreply.github.com>
2026-08-26 22:42:58 -0400

    add extra text to welcome message

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 def welcome_message(name):
-    return f"{name}, welcome to the Data Engineering course."
+    return f"{name}, welcome to the Data Engineering course. The course will be a great learning experience!"
 
 
 if __name__ == "__main__":
