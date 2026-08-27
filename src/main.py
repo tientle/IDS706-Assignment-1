cd3972e845a361defdc1f8fae3246487994e227f
Tien Le <71688550+tientle@users.noreply.github.com>
2026-08-26 23:41:54 -0400

    add favorite color function, add extra test to test file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 def welcome_message(name):
     return f"{name}, welcome to the Data Engineering course. The course will be a great learning experience!"
 
+def favorite_color(color):
+    return f"{color} is an amazing color!"
 
 if __name__ == "__main__":
     name = input("Enter your name: ")
     print(welcome_message(name))
+    color = input("Enter your favorite color: ")
+    print(favorite_color(color))
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,8 @@
 from src.main import welcome_message
+from src.main import favorite_color
 
 def test_welcome_message():
-    assert welcome_message("Ammy") == "Ammy, welcome to the Data Engineering course."
+    assert welcome_message("Ammy") == "Ammy, welcome to the Data Engineering course. The course will be a great learning experience!"
+
+def test_favorite_color():
+    assert favorite_color("Purple") == "Purple is an amazing color!"
