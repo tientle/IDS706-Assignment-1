f95af6586243b830f952eea66270fc903c603157
Tien Le <71688550+tientle@users.noreply.github.com>
2026-08-30 23:25:06 -0400

    adding comments to python file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
+# Function that greets user after inputting name
 def welcome_message(name):
     return f"{name}, welcome to the Data Engineering course. The course will be a great learning experience!"
 
+# Function that comments on color inputted by user
 def favorite_color(color):
     return f"{color} is an amazing color!"
 
